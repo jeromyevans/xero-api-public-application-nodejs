ac68b9944bf31027ff284b2d184a98b2b41d571c
Jeromy Evans (personal) <jeromy.evans@blueskyminds.com.au>
2015-06-09 16:44:50 +1000

    Added initial Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+ * quicklook-xero-client
+ *
+ */
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+  grunt.initConfig({
+
+    // Project metadata
+    pkg   : grunt.file.readJSON('package.json'),
+
+    // Lint JavaScript
+    jshint: {
+      all: ['Gruntfile.js', 'src/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    }
+
+  });
+    
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
